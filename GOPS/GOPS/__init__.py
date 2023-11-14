6044d51840492277d879fb4f40070f4cab4a2784
Francesco Carraro <francesco.carraro21@imperial.ac.uk>
2023-11-14 16:43:39 +0000

    modify init

--- GOPS/GOPS/__init__.py
+++ GOPS/GOPS/__init__.py
@@ -3,5 +3,6 @@ from .solve2by2 import solve2by2
 from .payout2by2 import payout2by2
 from .solve2byN import solve2byN
 from .solve3by3 import solve3by3
+from .solve4by4 import solve4by4
 from .payout3by3 import payout3by3
 from .payout4by4 import payout4by4
